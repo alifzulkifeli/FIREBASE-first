1d97ca78924bd73791d9f10e53d28b2cb2ccacd1
aliff <alifzulkifeli@gmail.com>
2020-11-05 02:42:58 +0900

    ADD:add starting project for my first firebase project

---
+++ firebae-01.md
@@ -0,0 +1,13 @@
+** starting an app
+
+- create account on firebase
+- copy sdk to the html
+- run firebase login 
+- run firebase init
+- choose service
+- run firebase start / firebase emulators:start to start app
+- run firabese deploy to deploy the apps to hosting
+
+** create login function
+- set login method
+- import firebase
---
+++ public/app.js
@@ -0,0 +1,12 @@
+const auth = firebase.auth()
+
+const whenSignenIn = document.getElementById("whenSignedIn")
+const whenSignedOut = document.getElementById("whenSignedOut")
+
+const signInBtn = document.getElementById("signInBtn")
+const signOutBtn = document.getElementById("signOutBtn")
+
+const userDetails = document.getElementById("userDetails")
+
+//to make user be able user google account in fire base
+const provider = new firebase.auth.GoogleAuthProvider()
