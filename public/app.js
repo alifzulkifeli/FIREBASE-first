b94f2122b5eb43248dc9f1b789b1876412a4ccdd
aliff <alifzulkifeli@gmail.com>
2020-11-07 04:00:24 +0900

    add the login function

--- public/app.js
+++ public/app.js
@@ -13,3 +13,18 @@ const provider = new firebase.auth.GoogleAuthProvider()
 
 signInBtn.onclick = () => auth.signInWithPopup(provider)
 signOutBtn.onclick = () => auth.signOut()
+
+//to make app react
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    //signed in
+    whenSignenIn.hidden = false;
+    whenSignedOut.hidden = true;
+    userDetails.innerHTML = `<h3>Hello ${user.displayName}</h3>, Id: ${user.uid}`;
+  } else {
+    //signed out
+    whenSignenIn.hidden = true;
+    whenSignedOut.hidden = false;
+    userDetails.innerHTML = ``;
+  }
+})
--- readme.md
+++ readme.md
@@ -5,9 +5,10 @@
 - run firebase login 
 - run firebase init
 - choose service
-- run firebase start / firebase emulators:start to start app
+- run firebase serve / firebase emulators:start to start app
 - run firabese deploy to deploy the apps to hosting
 
 ** create login function
 - set login method
 - import firebase
+- to create login and logout function
